218f0fe2cf4dd1b13a92c0c9887dd6dd5d224be8
Pau Perez <saulopefa@gmail.com>
2023-07-05 09:22:51 +0200

    Skip meta rows from Comdirect's CSV

--- load.py
+++ load.py
@@ -1,15 +1,36 @@
 import duckdb
+import sys
+
+if len(sys.argv) != 2:
+    raise ValueError("Unknown source. Please provide one of `tomorrow` or `comdirect`")
+
+source = sys.argv[1]
+source_filename = f"raw_{source}.csv"
+table_name = f"{source}.raw_transactions"
 
 connection = duckdb.connect(database="my_finance.duckdb")
 
-# replace the thousand separator and then the decimal one
-connection.execute(
-    """
-    CREATE SCHEMA IF NOT EXISTS tomorrow;
-    CREATE TABLE tomorrow.raw_transactions AS
-        SELECT
-            *,
-            replace(replace(amount, '.', ''), ',', '.') AS normalized_amount,
-        FROM read_csv_auto('raw_tomorrow.csv')
-    """
-)
+connection.execute(f"CREATE SCHEMA IF NOT EXISTS {source}")
+connection.execute(f"DROP TABLE IF EXISTS {table_name}")
+
+if source == "tomorrow":
+    # replace the thousand separator and then the decimal one
+    connection.execute(
+        f"""
+        CREATE TABLE {table_name} AS
+            SELECT
+                *,
+                replace(replace(amount, '.', ''), ',', '.') AS normalized_amount,
+            FROM read_csv_auto(?)
+        """,
+        [source_filename],
+    )
+elif source == "comdirect":
+    connection.execute(
+        f"""
+        CREATE TABLE {table_name} AS
+            SELECT *
+            FROM read_csv_auto(?, skip=4)
+        """,
+        [source_filename],
+    )
